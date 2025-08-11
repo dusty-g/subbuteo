360eb173257c7b52c85488d2bb9245d4116240c4
Dusty <dustygalindo@gmail.com>
2025-08-10 18:46:13 -0700

    added gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,7 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-export default defineConfig({ plugins: [svelte()] });
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+export default defineConfig({
+  plugins: [svelte()],
+  base: '/' // using a custom domain, so serve from root
+})
